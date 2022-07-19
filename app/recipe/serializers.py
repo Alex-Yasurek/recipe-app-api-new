7dbb08d07d2832bfb3b6ccb16394c67626eebb1c
Alex <ayasurek@gmail.com>
2022-07-18 22:09:50 -0400

    added detail view and serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,5 +8,19 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
+        # includes all fields from model except description
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view.
+    This inherits from recipeSerializer, which inherits
+    from ModelSerializer.
+    DIfference is this serializer will include descriptions"""
+
+    # inherit from RecipeSerializer.Meta to pull in all the fields
+    # listed there
+    class Meta(RecipeSerializer.Meta):
+        # we want fields inherited plus add description
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,4 @@
 """Views for the recipe APIs"""
-
-from tokenize import Token
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -11,7 +9,8 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
-    serializer_class = serializers.RecipeSerializer
+    # default serializer to use for all api calls
+    serializer_class = serializers.RecipeDetailSerializer
     # this specifies what model (objects) to use for the viewset
     queryset = Recipe.objects.all()
     # need to use token auth to access these apis
@@ -23,3 +22,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Override queryset to only pull recipes
         of current user"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request.
+        This func returns a reference to a class so it can instantiate it."""
+        # when list api is called (GET), use a different
+        # serializer than default
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        # everything else use default
+        return self.serializer_class
