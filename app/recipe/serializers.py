3e247dc60a293cb31c949b4151b044363b4f6979
Alex <ayasurek@gmail.com>
2022-07-29 13:37:23 -0400

    add create tag feature to recipe, nested serializer for recipe, and tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,14 +3,46 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes"""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
         # includes all fields from model except description
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe"""
+        # remove tags from data
+        tags = validated_data.pop('tags', [])
+        # create recipe without tags data since tags have
+        # to be added afterwards
+        recipe = Recipe.objects.create(**validated_data)
+        # get user who made call
+        auth_user = self.context['request'].user
+
+        for tag in tags:
+            # create or retrieve tag if already exists
+            tags_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                # **tag future proofs it incase we add new things tags
+                **tag
+            )
+            # add tag to recipe
+            recipe.tags.add(tags_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -24,12 +56,3 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         # we want fields inherited plus add description
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags"""
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
